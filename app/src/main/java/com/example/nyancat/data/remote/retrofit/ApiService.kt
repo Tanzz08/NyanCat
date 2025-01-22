4a4c0b7816591eabeff333d3f1621e136f4e3f58
tanzz <bustanuldarking23@gmail.com>
2025-01-22 21:14:02 +0800

    function untuk endpoint dari TheCatApi

--- app/src/main/java/com/example/nyancat/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/example/nyancat/data/remote/retrofit/ApiService.kt
@@ -1,10 +1,17 @@
 package com.example.nyancat.data.remote.retrofit
 
 import com.example.nyancat.data.remote.response.TheCatApiResponse
+import com.example.nyancat.data.remote.response.TheCatApiResponseItem
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("images/search?limit=10&breed_ids=beng")
-    suspend fun getData(@Query("api_key") apiKey: String): TheCatApiResponse
+    @GET("images/search?limit=4&breed_ids=aege")
+    suspend fun getData(@Query("api_key") apiKey: String): List<TheCatApiResponseItem>
+
+    @GET("images/search?limit=4&breed_ids=beng")
+    suspend fun bengCats(@Query("api_key") apiKey: String): List<TheCatApiResponseItem>
+
+    @GET("images/search?limit=4&breed_ids=asho")
+    suspend fun ashoCats(@Query("api_key") apiKey: String): List<TheCatApiResponseItem>
 }
